3d03965976e24b0fb937ab2ee6473e81afaab508
wizardto1 <ph429@drexel.edu>
2020-07-22 17:01:49 -0400

    6 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,14 +24,15 @@ import { Survey11Component } from './survey11/survey11.component';
 import { Survey12Component } from './survey12/survey12.component';
 import { Survey13Component } from './survey13/survey13.component';
 import { Survey14Component } from './survey14/survey14.component';
+import { Warning1Component } from './warning1/warning1.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo : '/home', pathMatch:'full' },{path:'home', component: HomeComponent },{path:'survey1',component:Survey1Component, pathMatch:'full'},{path:'survey2',component:Survey2Component},{path:'survey3',component:Survey3Component},{path:'survey4',component:Survey4Component},{path:'survey5',component:Survey5Component},{path:'survey6',component:Survey6Component},{path:'survey7',component:Survey7Component},{path:'survey8',component:Survey8Component},{path:'survey9',component:Survey9Component},{path:'survey10',component:Survey10Component},{path:'survey11',component:Survey11Component},{path:'survey12',component:Survey12Component},{path:'survey13',component:Survey13Component},{path:'survey14',component:Survey14Component},{path:'results',component:ResultsComponent}
+  { path: '', redirectTo : '/home', pathMatch:'full' },{path:'home', component: HomeComponent },{path:'survey1',component:Survey1Component, pathMatch:'full'},{path:'survey2',component:Survey2Component},{path:'survey3',component:Survey3Component},{path:'survey4',component:Survey4Component},{path:'survey5',component:Survey5Component},{path:'survey6',component:Survey6Component},{path:'survey7',component:Survey7Component},{path:'survey8',component:Survey8Component},{path:'survey9',component:Survey9Component},{path:'survey10',component:Survey10Component},{path:'survey11',component:Survey11Component},{path:'survey12',component:Survey12Component},{path:'survey13',component:Survey13Component},{path:'survey14',component:Survey14Component},{path:'warning1',component:Warning1Component},{path:'results',component:ResultsComponent}
 ];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,  RouterModule.forRoot(appRoutes) ],
-  declarations: [ AppComponent,  HomeComponent, Survey1Component, Survey2Component, Survey3Component, ResultsComponent, Survey4Component, BodyComponent, TimerComponent, HeaderComponent, Survey5Component, Survey6Component, Survey7Component, Survey8Component, Survey9Component, Survey10Component, Survey11Component, Survey12Component, Survey13Component, Survey14Component ],
+  declarations: [ AppComponent,  HomeComponent, Survey1Component, Survey2Component, Survey3Component, ResultsComponent, Survey4Component, BodyComponent, TimerComponent, HeaderComponent, Survey5Component, Survey6Component, Survey7Component, Survey8Component, Survey9Component, Survey10Component, Survey11Component, Survey12Component, Survey13Component, Survey14Component, Warning1Component ],
   bootstrap:    [ AppComponent ],
   providers: [DataService]
 })
---
+++ src/app/warning1/warning1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning1',
+  templateUrl: './warning1.component.html',
+  styleUrls: ['./warning1.component.css']
+})
+export class Warning1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
