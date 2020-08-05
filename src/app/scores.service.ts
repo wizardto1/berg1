0e1733cdabf1ab0144eb0394a44a2701bfc4de97
wizardto1 <ph429@drexel.edu>
2020-08-05 16:00:32 -0400

    2 files modified

--- src/app/scores.service.ts
+++ src/app/scores.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@angular/core';
+import { AppModule } from './app.module';
 
 @Injectable()
 export class ScoresService {
+  scores=[];
   score1:any;
   score2:any;
+  score3:any;
+  score4:any;
+  score5:any;
+  score6:any;
+  score7:any;
+  score8:any;
+  score9:any;
+  score10:any;
+  score11:any;
+  score12:any;
+  score13:any;
+  score14:any;
   button1:boolean;
+  button2:boolean;
+  button3:boolean;
+  button4:boolean;
+  button5:boolean;
+  button6:boolean;
+  button7:boolean;
+  button8:boolean;
+  button9:boolean;
+  button10:boolean;
+  button11:boolean;
+  button12:boolean;
+  button13:boolean;
+  button14:boolean;
   total;
   max;
   setScore1(score){
@@ -20,6 +47,7 @@ export class ScoresService {
   getButton1(){
     return this.button1;
   }
+
   constructor() { }
 
 }
