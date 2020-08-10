53a8c86ca1866d8fc214169de59f489b43434e57
wizardto1 <ph429@drexel.edu>
2020-08-10 11:52:59 -0400

    4 files modified

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -11,5 +11,46 @@ export class TimerComponent implements OnInit {
 
   ngOnInit() {
   }
+time: number = 0;
+  minutes:number=  0;
+  seconds:number= 0;
+  
+interval;
 
+
+start() {
+  this.play = true;
+  this.interval = setInterval(() => {
+    this.time++;
+    
+        this.minutes = Math.floor(this.time / 60);
+        console.log(this.minutes)-(this.minutes * 60)
+        this.seconds = this.time-(this.minutes * 60)
+        console.log(this.seconds)
+        /** 
+        if (Number(this.minutes) < 10) {
+          this.minutes = '0' + this.minutes;
+        } else {
+          this.minutes = '' + this.minutes;
+        }
+        if (Number(this.seconds) < 10) {
+          this.seconds = '0' + this.seconds;
+        } else {
+          this.seconds = '' + this.seconds;
+        }*/
+        
+  },1000)
+}
+
+stop() {
+  this.play = false;
+  clearInterval(this.interval);
+}
+reset(){
+  this.play=false;
+  clearInterval(this.interval);
+  this.time=0;
+  this.seconds=0;
+  this.minutes=0;
+}
 }
